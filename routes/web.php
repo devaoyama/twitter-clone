ad222445fd83124cfed1ccea5fac65b2361fb760
devkeita <keita.mywork@gmail.com>
2021-06-09 17:23:07 +0900

    update route for MessageController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DefaultController;
+use App\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,12 @@ use App\Http\Controllers\DefaultController;
 */
 
 Route::get('/', [DefaultController::class, 'index'])->name('default');
+
+Route::post('/messages', [MessageController::class, 'store'])
+    ->middleware('can:store,App\Models\Message');
+Route::post('/messages/{message}', [MessageController::class, 'update'])
+    ->middleware('can:update,message');
+Route::delete('/messages/{message}', [MessageController::class, 'destroy'])
+    ->middleware('can:destroy,message');
 
 require __DIR__ . '/auth.php';
