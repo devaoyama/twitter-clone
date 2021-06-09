afbafe0cc5cf5cc899ef884ad3e5b3824504b94f
devkeita <keita.mywork@gmail.com>
2021-06-09 15:57:03 +0900

    update order by desc for messages

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
     public function index()
     {
-        $messages = Message::orderBy('id')->cursorPaginate(10);
+        $messages = Message::orderBy('id', 'DESC')->cursorPaginate(10);
 
         return view('default.index', compact('messages'));
     }
