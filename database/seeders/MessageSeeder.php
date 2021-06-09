e3caec0a4f0ccd6dd0817c08fc4718d544d67a39
devkeita <keita.mywork@gmail.com>
2021-06-09 14:04:11 +0900

    add factory and seeder for Message model

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class MessageSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(MessageSeeder::class);
+        Message::factory(10)->create();
     }
 }
