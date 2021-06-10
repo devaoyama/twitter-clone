2b73ee98702bcc9109d1d6903119f7e4305be8f0
devkeita <keita.mywork@gmail.com>
2021-06-10 14:27:55 +0900

    add views for edit messages

--- resources/views/components/message-card.blade.php
+++ resources/views/components/message-card.blade.php
@@ -11,6 +11,9 @@
                         <button class="ml-2 text-lg font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">︙</button>
                     </x-slot>
                     <x-slot name="content">
+                        <x-dropdown-link class="text-green-600" href="{{ route('messages.edit', ['message' => $message->id]) }}">
+                            編集
+                        </x-dropdown-link>
                         <form method="POST" action="{{ route('messages.destroy', ['message' => $message->id]) }}">
                             @csrf
 
