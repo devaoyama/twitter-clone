0be74c7472c096e8d8416c2d0ad62c939b313e5e
devkeita <keita.mywork@gmail.com>
2021-06-14 18:37:10 +0900

    update permission owner from nginx to ec2-user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-chown -R ec2-user /var/www/twitter-clone
-
 composer install --optimize-autoloader --no-dev
 npm install
 npm run prod
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
-
-chown -R nginx /var/www/twitter-clone
