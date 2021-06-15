9598f402db3edbd49037f4445e2f231cce3161bd
devkeita <keita.mywork@gmail.com>
2021-06-15 18:52:54 +0900

    message cardとloadingのデザインを調整

--- resources/views/default/index.blade.php
+++ resources/views/default/index.blade.php
@@ -4,10 +4,10 @@
             @auth
                 <x-message-create-form :content="old('content')" />
             @endauth
-            <div class="p-8" x-data="infiniteScroll()" x-init="init()">
+            <div x-data="infiniteScroll()" x-init="init()">
                 <x-message-cards />
 
-                <div class="bg-white h-64 w-full flex text-pink-600 items-center justify-center" id="infinite-scroll-trigger">
+                <div class="bg-white h-64 flex text-pink-600 items-center justify-center mx-3 my-5 rounded-lg shadow-md" id="infinite-scroll-trigger">
                     <svg class="animate-spin -ml-1 mr-3 h-5 w-5 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
