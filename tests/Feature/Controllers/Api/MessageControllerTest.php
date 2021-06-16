2c229e77335a2b5d13f5a936332120d94939c27a
devkeita <keita.mywork@gmail.com>
2021-06-16 13:22:46 +0900

    messageを変更するapiのテストを作成

--- tests/Feature/Controllers/Api/MessageControllerTest.php
+++ tests/Feature/Controllers/Api/MessageControllerTest.php
@@ -5,13 +5,14 @@ namespace Tests\Feature\Controllers\Api;
 use App\Http\Resources\MessageResource;
 use App\Models\Message;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class MessageControllerTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     public function test_messages_can_be_returned()
     {
@@ -23,6 +24,71 @@ class MessageControllerTest extends TestCase
             json_encode($response->json('data'))
         );
         $response->assertSuccessful();
+    }
+
+    public function test_message_can_be_updated()
+    {
+        $message = Message::factory()->create();
+        $response = $this->actingAs($message->user)->put('/api/messages/' . $message->id, [
+            'content' => 'updated content',
+        ]);
+
+        // Messageが更新されたかどうか
+        $updatedMessage = Message::find($message->id);
+        $this->assertEquals('updated content', $updatedMessage->content);
+
+        // レスポンスが正しいかどうか
+        $response->assertSuccessful();
+        $this->assertEquals(
+            MessageResource::make($updatedMessage)->toJson(),
+            json_encode($response->json('data'))
+        );
+    }
+
+    public function test_message_can_not_be_updated_if_content_empty()
+    {
+        $message = Message::factory()->create();
+        $response = $this->actingAs($message->user)->put('/api/messages/' . $message->id, [
+            'content' => '',
+        ]);
+
+        // Messageが更新されていないかどうか
+        $updatedMessage = Message::find($message->id);
+        $this->assertNotEquals('updated content', $updatedMessage->content);
+
+        $response->assertJsonValidationErrors(['content' => 'メッセージは、必ず指定してください。']);
+        $response->assertStatus(400);
+    }
+
+    public function test_message_can_not_be_updated_if_content_long()
+    {
+        $message = Message::factory()->create();
+        $response = $this->actingAs($message->user)->put('/api/messages/' . $message->id, [
+            'content' => $this->faker->realTextBetween(),
+        ]);
+
+        // Messageが更新されていないかどうか
+        $updatedMessage = Message::find($message->id);
+        $this->assertNotEquals('updated content', $updatedMessage->content);
+
+        $response->assertJsonValidationErrors(['content' => 'メッセージは、140文字以下にしてください。']);
+        $response->assertStatus(400);
+    }
+
+    public function test_message_can_not_be_updated_if_unauthorized()
+    {
+        $message = Message::factory()->create();
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->put('/api/messages/' . $message->id, [
+            'content' => 'updated content',
+        ]);
+
+        // Messageが更新されていないかどうか
+        $updatedMessage = Message::find($message->id);
+        $this->assertNotEquals('updated content', $updatedMessage->content);
+
+        // レスポンスが正しいかどうか
+        $response->assertStatus(403);
     }
 
     public function test_message_can_be_destroyed()
